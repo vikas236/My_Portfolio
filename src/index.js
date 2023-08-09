3ac334de604e536a5583fa30ec4f7e38570da991
vikas236 <saivikas461@gmail.com>
2023-08-09 18:05:40 +0530

    saving cause I'm switching distros

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Import css styling
+import "./css/styles.css";
+
+// Import Images
+import "./images/laptop.jpg";
+
+const intro = document.querySelector(".intro");
+// intro.style.background = "url('/dist/images/laptop.jpg')";
+intro.style.background = "grey";
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Portfolio",
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "[name][ext]",
+        },
+      },
+    ],
+  },
+};
